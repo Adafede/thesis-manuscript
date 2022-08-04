976f9987e40be9ac6a3e51a5682276100ebc7d10
Adafede <adriano.rutz@hotmail.com>
2022-08-04 20:26:29 +0200

    feat(build, pandoc): update citation backlinks filters to include inline citations and improve aggregation by compound classes

---
+++ build/pandoc/filters/inline-citation-backlinks.lua
@@ -0,0 +1,62 @@
+-- lua filter to add backlinks to citations
+-- run after --citeproc on the command line
+
+-- cites is a table mapping citation item identifiers
+-- to an array of cite identifiers
+local cites = {}
+
+-- counter for cite identifiers
+local cite_number = 1
+
+local function with_latex_label(s, el)
+  if FORMAT == "latex" then
+    return {pandoc.RawInline("latex", "\\label{" .. s .. "}"), el}
+  else
+    return {el}
+  end
+end
+
+function Cite(el)
+  local cite_id = "cite_" .. cite_number
+  cite_number = cite_number + 1
+  for _,citation in ipairs(el.citations) do
+    if cites[citation.id] then
+      table.insert(cites[citation.id], cite_id)
+    else
+      cites[citation.id] = {cite_id}
+    end
+  end
+  return pandoc.Span(with_latex_label(cite_id, el), pandoc.Attr(cite_id))
+end
+
+function append_inline(blocks, inlines)
+  local last = blocks[#blocks]
+  if last.t == 'Para' or last.t == 'Plain' then
+    -- append to last block
+    last.content:extend(inlines)
+  else
+    -- append as additional block
+    blocks[#blocks + 1] = pandoc.Plain(inlines)
+  end
+  return blocks
+end
+
+function Div(el)
+  local citation_id = el.identifier:match("ref%-(.+)")
+  if citation_id then
+    local backlinks = pandoc.Inlines{pandoc.Str("Cited on:"),pandoc.Space()}
+    for i,cite_id in ipairs(cites[citation_id]) do
+      local marker = pandoc.Str(i)
+      if FORMAT == "latex" then
+        marker = pandoc.RawInline("latex", "\\autopageref*{" .. cite_id .. "}")
+      end
+      if #backlinks > 2 then
+        table.insert(backlinks, pandoc.Str(","))
+        table.insert(backlinks, pandoc.Space())
+      end
+      table.insert(backlinks, pandoc.Link(marker, "#"..cite_id))
+    end
+    append_inline(el.content, {pandoc.Space()} .. backlinks)
+    return el
+  end
+end
