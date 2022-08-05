c9793bd7d6f80c4d80c4cb7c39753752f78739e1
Adafede <adriano.rutz@hotmail.com>
2022-08-05 10:02:30 +0200

    update

--- build/build_2.sh
+++ build/build_2.sh
@@ -19,9 +19,23 @@ PANDOC_DATA_DIR="${PANDOC_DATA_DIR:-build/pandoc}"
 if [ -d output/images ]; then rm -rf output/images; fi  # if images is a directory, remove it
 cp -R -L content/images ./
 pandoc \
+output/manuscript.md \
 --output=output/thesis.pdf \
 --template=build/assets/template.tex \
 --include-in-header=build/assets/preamble.tex \
+--bibliography=output/references.json \
+--csl=build/assets/style.csl \
+--filter=pandoc-xnos \
+--filter=pandoc-fignos \
+--filter=pandoc-eqnos \
+--filter=pandoc-tablenos \
+--filter=pandoc-secnos \
+--filter=pandoc-manubot-cite \
+--citeproc \
+--lua-filter=build/pandoc/filters/inline-citation-backlinks.lua \
+--pdf-engine=xelatex \
+--metadata link-citations=true \
+--number-sections \
 --variable mainfont="Palatino" \
 --variable sansfont="Helvetica" \
 --variable monofont="Menlo" \
@@ -29,23 +43,10 @@ pandoc \
 --variable=fontsize:12pt \
 --variable=papersize:a4paper \
 --variable=documentclass:report \
---pdf-engine=xelatex \
-output/manuscript.md \
---filter=pandoc-xnos \
---filter=pandoc-fignos \
---filter=pandoc-eqnos \
---filter=pandoc-tablenos \
---filter=pandoc-secnos \
---filter=pandoc-manubot-cite \
---lua-filter=build/pandoc/filters/short-captions.lua \
---number-sections \
---bibliography=output/references.json \
---citeproc \
---lua-filter=build/pandoc/filters/inline-citation-backlinks.lua \
---csl=build/assets/style.csl \
---metadata link-citations=true \
+
 --verbose \
 2>pandoc.pdf.log
 rm -rf ./images
 
 echo >&2 "Build complete"
+
